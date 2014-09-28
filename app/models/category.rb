0afb436fd38d35f4bafdc1f0f20fb4502dbcb55c
Adrian Cunanan <adrian@adriancunanan.com>
2014-09-28 19:38:16 -0400

    Add Seed Data

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,7 +2,5 @@ class Category < ActiveRecord::Base
 	has_many :article_categories
 	has_many :articles, through: :article_categories
 
-	validates :name, 
-						presence: true,
-						uniqueness: true
+	validates :name, uniqueness: true
 end
