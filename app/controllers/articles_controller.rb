07663d5a03b0d9cef85e4fad152d3aae697e8830
Adrian Cunanan <adrian@adriancunanan.com>
2014-12-11 09:59:34 -0500

    Display 25 articles in index view

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,14 +1,15 @@
- class ArticlesController < ApplicationController
-   def index
-   end
+class ArticlesController < ApplicationController
+  def index
+		@articles = Article.order(updated_at: :desc).limit(25)
+	end
 
-   def show
-   end
+	def show
+	end
 
-   def new
-   end
+	def new
+	end
 
-   def create
-   end
+	def create
+	end
 
- end
+end
